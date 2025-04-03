6a3bf7e60c3976dca31fdb360e12c44840902ffd
Dakari Sow <dddddakari@gmail.com>
2025-04-03 15:52:54 -0600

    New Exception Class

---
+++ src/exceptions/EmptyStackException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class EmptyStackException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public EmptyStackException() {
+        super();
+    }
+
+    public EmptyStackException(String message) {
+        super(message);
+    }
+}
