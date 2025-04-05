511d0cf32a1536b34457fc3c8175555fcdba8592
Dakari Sow <dddddakari@gmail.com>
2025-04-04 22:09:49 -0600

    EmptyStackException Work

--- src/exceptions/EmptyStackException.java
+++ src/exceptions/EmptyStackException.java
@@ -1,10 +1,17 @@
 package exceptions;
 
 public class EmptyStackException extends RuntimeException {
+<<<<<<< Updated upstream
     private static final long serialVersionUID = 1L;
     
     public EmptyStackException() {
         super("Stack is empty");
+=======
+	private static final long serialVersionUID = 1L;
+
+	public EmptyStackException() {
+        super("Stack is empty.");
+>>>>>>> Stashed changes
     }
     
     public EmptyStackException(String message) {
